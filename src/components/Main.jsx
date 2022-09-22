b9ee8fde1f273bd10117d43da36d53ee9898e3d5
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-09-22 12:13:48 +0530

    added framer motion animations

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { motion } from "framer-motion";
 import ReactMarkdown from "react-markdown"
-const Main = ({activeNote, onUpdateNote}) => {
+const Main = ({ activeNote, onUpdateNote }) => {
 
-  const onEditField = (key,value) => {
+  const onEditField = (key, value) => {
     onUpdateNote({
       ...activeNote,
       [key]: value,
-      lastModified:Date.now(),
+      lastModified: Date.now(),
     });
   }
 
@@ -15,19 +16,24 @@ const Main = ({activeNote, onUpdateNote}) => {
   </div>;
   return (
     <div
+
       className='flex-1 h-screen'>
-      <div className='h-fit w-full'>
-        <form className='flex flex-col items-start space-y-4'>
+      <div
+
+        className='h-fit w-full'>
+        <motion.form initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration:1 }} className='flex flex-col items-start space-y-4'>
           <input value={activeNote.title} onChange={(e) => onEditField("title", e.target.value)} autoFocus type="text" placeholder='Enter title for you note' className='outline-none p-4 rounded-md drop-shadow-md    w-[90%] mx-auto mt-2 text-3xl font-light bg-gray-200' />
           <textarea name="" id="" value={activeNote.body} onChange={(e) => onEditField("body", e.target.value)} className='border border-gray-200 w-[90%] h-[40vh] p-5 mx-auto rounded-md outline-none text-base bg-gray-100' placeholder='Enter text to add to this note'>{activeNote.body}</textarea>
-        </form>
+        </motion.form>
         <div className='bg-blue-500 flex  flex-col text-white ml-16 mt-5 p-3 w-1/2 rounded-t-md'>
-        <h2 className='font-bold'>{activeNote.title} 
-        </h2>
+          <h2 className='font-bold'>{activeNote.title}
+          </h2>
         </div>
         <div className='bg-gray-300 w-1/2  ml-16 h-fit p-5 rounded-b-md shadow-md'>
-        <h3 className='mt-4 font-semibold'>Preview</h3>
-        <ReactMarkdown>{activeNote.body.length > 0 ? activeNote.body : <p className='text-base font-light'>No text to preview </p> }</ReactMarkdown>
+          <h3 className='mt-4 font-semibold'>Preview</h3>
+          <ReactMarkdown>{activeNote.body.length > 0 ? activeNote.body : <p className='text-base font-light'>No text to preview </p>}</ReactMarkdown>
         </div>
       </div>
     </div>
